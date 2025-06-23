2baa007e3a4055c5521e6390eb4413f9d28c59e4
Eduardo Lopes <eduardolopes3574@gmail.com>
2025-06-23 19:39:22 -0300

    Project init

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:ecommerce_app/utils/theme/custom_theme/text_theme.dart';
+import 'package:flutter/material.dart';
+
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
